629101e43b5dfd923332ec3d9467c283d35a3bc6
Julio Gonzalez Altamirano <devjga@gmail.com>
2015-08-10 09:28:24 -0500

    Create tests folder.

---
+++ tests/test_risk.py
@@ -0,0 +1,19 @@
+import unittest
+from ballotbleach import risk
+
+
+class CheckChainStuffingTests(unittest.TestCase):
+    pass
+
+
+class CheckVerbosityTests(unittest.TestCase):
+    pass
+
+
+class CheckCompletionTests(unittest.TestCase):
+    pass
+
+
+class CheckCommentDuplicationTests(unittest.TestCase):
+    pass
+
