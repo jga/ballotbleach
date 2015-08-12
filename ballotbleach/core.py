0a36f8d4908aa428d942a6f9b010e6b7af441ed0
Julio Gonzalez Altamirano <devjga@gmail.com>
2015-08-11 19:31:12 -0500

    Add logic to risk functions.

--- ballotbleach/core.py
+++ ballotbleach/core.py
@@ -1,13 +1,16 @@
+from datetime import datetime
 from logging import getLogger, config
 from os.path import expanduser
+import pytz
 import xlrd
 from .classes import Ballot, Store
 
 logger = getLogger(__name__)
 
-
+BALLOTBLEACH_TIMEZONE_NAME = 'America/Chicago'
 USER_HOME = expanduser('~')
 DATA_FILE_PATH = ''.join((USER_HOME, '/dev/data/council_survey_newest.xlsx',))
+OUT_FILE_DIRECTORY = ''.join((USER_HOME, '/dev/data/'))
 LOGGER_CONFIG = {
     'version': 1,
     'disable_existing_loggers': True,
@@ -18,7 +21,7 @@ LOGGER_CONFIG = {
         'simple': {
             'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
         },
-        },
+    },
     'handlers': {
         'console': {
             'level': 'DEBUG',
@@ -31,29 +34,56 @@ LOGGER_CONFIG = {
             'handlers': ['console'],
             'propagate': True,
             'level': 'DEBUG',
-            },
-        }
+        },
+    }
 }
 
 
 def row_to_ballot(row):
-    ballot = Ballot(row[0].value, row[1].value,
-                    row[2].value, row[3].value)
+    tz = pytz.timezone(BALLOTBLEACH_TIMEZONE_NAME)
+    excel_timestamp = xlrd.xldate_as_tuple(row[0].value, 0)
+    logger.debug(excel_timestamp)
+    timestamp = datetime(excel_timestamp[0], excel_timestamp[1], excel_timestamp[2],
+                         excel_timestamp[3], excel_timestamp[4], excel_timestamp[5],
+                         tzinfo=tz)
+    most_effective = row[3].value if row[3].value else 'None'
+    ballot = Ballot(timestamp, row[1].value,
+                    row[2].value, most_effective)
     return ballot
 
 
-def load_ballots(filename):
+def load_ballots(filename, skip_first_row=True):
+    """
+    Adds ballots from a passed file.
+    """
     store = Store()
     book = xlrd.open_workbook(filename=filename)
     sheet = book.sheet_by_index(0)
-    logger.info('Total filled rows {0}'.format(sheet.nrows))
-    for row in range(1, sheet.nrows):
+    logger.debug('Total filled rows {0}'.format(sheet.nrows))
+    start_row = 1 if skip_first_row else 0
+    for row in range(start_row, sheet.nrows):
         ballot = row_to_ballot(sheet.row(row))
         store.add_ballot(ballot)
     return store
 
 
 def run():
+    """
+    Called by command line script per setup.py configuration. Writes out
+    a CSV with all ballots scored for risk.
+    """
     config.dictConfig(LOGGER_CONFIG)
     store = load_ballots(DATA_FILE_PATH)
-    store.print_all_ballots()
+    store.score_risk()
+    store.to_csv(OUT_FILE_DIRECTORY, 'all_ballots.csv')
+
+
+def dump_clean():
+    """
+    Called by command line script per setup.py configuration. Writes out
+    a CSV with all ballots scored for risk.
+    """
+    config.dictConfig(LOGGER_CONFIG)
+    store = load_ballots(DATA_FILE_PATH)
+    store.score_risk()
+    store.to_csv(OUT_FILE_DIRECTORY, 'clean_ballots.csv', 75)
