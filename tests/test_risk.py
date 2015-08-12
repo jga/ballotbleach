5a2df40bcef983cde1c16808ff9b88bc4d0820d0
Julio Gonzalez Altamirano <devjga@gmail.com>
2015-08-11 19:32:24 -0500

    Create Store for testing.

--- tests/test_risk.py
+++ tests/test_risk.py
@@ -1,19 +1,44 @@
+from datetime import datetime
 import unittest
+from ballotbleach import classes
 from ballotbleach import risk
 
 
+class TestStore(classes.Store):
+
+    def __init__(self, risk_assessments=None):
+        super(TestStore, self).__init__(risk_assessments=risk_assessments)
+        self.add_ballot(classes.Ballot(datetime.now(), 10, 'Housing, transportation, taxes', 'Washington'))
+        self.add_ballot(classes.Ballot(datetime.now(), 9, 'Policing, noise, taxes', 'Lincoln'))
+        self.add_ballot(classes.Ballot(datetime.now(), 8, 'Trees, water, sidewalks', 'Polk'))
+        self.add_ballot(classes.Ballot(datetime.now(), 8, 'Trees, water, sidewalks', 'Polk'))
+        self.add_ballot(classes.Ballot(datetime.now(), 6, 'Affordability', 'Roosevelt'))
+        self.add_ballot(classes.Ballot(datetime.now(), 5, 'Affordability', 'Roosevelt'))
+        self.add_ballot(classes.Ballot(datetime.now(), 5, 'Growth', 'Johnson'))
+        self.add_ballot(classes.Ballot(datetime.now(), 3, '', 'Johnson'))
+        self.add_ballot(classes.Ballot(datetime.now(), 1, 'Planning and policy', 'Johnson'))
+        self.add_ballot(classes.Ballot(datetime.now(), 1, 'Gentrification', 'Obama'))
+
+
 class CheckChainStuffingTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.store = TestStore()
 
 
 class CheckVerbosityTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.store = TestStore()
 
 
 class CheckCompletionTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.store = TestStore()
 
 
 class CheckCommentDuplicationTests(unittest.TestCase):
-    pass
 
+    def setUp(self):
+        self.store = TestStore()
